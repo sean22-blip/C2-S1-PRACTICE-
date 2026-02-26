0b42e8aaa67d22834eb1983bff8a6e0dac342200
sean22-blip <iseandxd@gmail.com>
2026-02-26 13:56:00 +0700

    modified:   exercise-1/exercise1/src/App.jsx 	modified:   exercise-1/exercise1/src/components/Scores.jsx

--- exercise-1/exercise1/src/App.jsx
+++ exercise-1/exercise1/src/App.jsx
@@ -1,13 +1,16 @@
-import pnLogo from "./assets/pn-logo.png";
+// import pnLogo from "./assets/pn-logo.png";
 import Header from "./components/Header";
 import Scores from "./components/Scores";
-import {HTML_RESULTS} from "./data.js";
+import { ENGLISH_RESULTS, HTML_RESULTS, JAVA_RESULTS, PYTHON_RESULTS } from "./data.js";
 function App() {
   return (
     <>
       <Header batchName="Generation 11" />
       <main className="scores-container">
+        <Scores courseName="JAVA"  courseResults={JAVA_RESULTS}></Scores>
         <Scores courseName="HTML" courseResults={HTML_RESULTS}></Scores>
+        <Scores courseName="PYTHON" courseResults={PYTHON_RESULTS}></Scores>
+        <Scores courseName="ENGLISH" courseResults={ENGLISH_RESULTS}></Scores>
       </main>
     </>
   );
--- exercise-1/exercise1/src/components/Scores.jsx
+++ exercise-1/exercise1/src/components/Scores.jsx
@@ -1,35 +1,32 @@
-import { HTML_RESULTS} from "../data";
-function Scores(props){
-return(
-      <div className="scores">
-          <h1>{props.courseName}</h1>
-
-    { myConcepts = myArray.map((myItem) => (
-          <table>
-            <thead>
-              <tr>
-                <th>{myItem.firstName}</th>
-                <th>{myItem.lastName}</th>
-                <th>{myItem.score}</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>First name 1 </td>
-                <td>Last name 1 </td>
-                <td>55</td>
-              </tr>
-              <tr>
-                <td>First name 2 </td>
-                <td>Last name 2 </td>
-                <td>45</td>
-              </tr>
-            </tbody>
-          </table>
-    ))   
+import { HTML_RESULTS } from "../data";
+import { JAVA_RESULTS } from "../data";
+import { PYTHON_RESULTS } from "../data";
+import { ENGLISH_RESULTS } from "../data";
+function Scores(props) {
+  return (
+    <div className="scores">
+      <h1>{props.courseName}</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>First name</th>
+            <th>Last name </th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.courseResults.map((data, index) => (
+            <tr key={index}>
+              <td>{data.firstName}</td>
+              <td>{data.lastName}</td>
+              <td className={data.score < 50? "warning": data.score}>{data.score}</td>
+              {/* <td className="warning">{data.score} {data.score < 50 ? false : true }</td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
-        </div>
 // {/* <Scores courseName="HTML" courseResults={HTML_RESULTS}></Scores> */}
-)
-}
 export default Scores;
